ebe466bc3410cab3e95021a4fae1cb20470ab6a3
Akshay Bodla <akshaybodla21@gmail.com>
2024-10-03 17:47:57 -0700

    fixed bugs

--- lidar_service/lidar_service/urls.py
+++ lidar_service/lidar_service/urls.py
@@ -19,8 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("controller.urls"))
+    path('', include("controller.urls")),
 
     # third party
-    path('api-auth/', include('rest_framework.urls))
+    path('api-auth/', include('rest_framework.urls')),
 ]
